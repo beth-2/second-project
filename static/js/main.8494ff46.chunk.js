082f9b326f40173f9aa236c8de65be8c8925f2e3
beth-2 <77358378+beth-2@users.noreply.github.com>
2021-04-20 00:44:50 +0300

    Updates

---
+++ static/js/main.8494ff46.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpsecond-project"]=this["webpackJsonpsecond-project"]||[]).push([[0],{4:function(e,t,c){"use strict";c.r(t);c(1);var n=c(3),s=c.n(n),r=c(0),o=function(){return Object(r.jsx)("div",{children:Object(r.jsx)("h1",{style:{textAlign:"center"},children:"This is Season App"})})};s.a.render(Object(r.jsx)(o,{}),document.querySelector("#root"))}},[[4,1,2]]]);
+//# sourceMappingURL=main.8494ff46.chunk.js.map
